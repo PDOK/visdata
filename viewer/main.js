01a701deddb01f302d1d6cc1b35af3e70a53a8df
NieneB <niene.boeijen@gmail.com>
2018-01-18 09:26:04 +0100

    viewer to see vector tiles

---
+++ viewer/main.js
@@ -0,0 +1,23 @@
+
+var bounds = [
+  [2.59326, 50.72915], // Southwest coordinates
+  [8.85636, 54.76957]  // Northeast coordinates
+];
+
+// MAP OPTIONS
+var options = {
+  container: 'map',
+  hash: true,
+  style: 'achtergrond.json',
+  zoom: 8,
+  maxBounds: bounds,
+  pitch: 0,
+  bearing: 0,
+  center: [5.19,52.33],
+  attributionControl: false
+}
+
+// INITIALIZE MAP
+var map = new mapboxgl.Map(options);
+map.addControl(new mapboxgl.NavigationControl(), 'top-left'); 
+map.addControl(new mapboxgl.AttributionControl(), 'bottom-left');
