1c97b6905bd03ac4572def3440d439b4304ac9b2
Frank Steggink <frank@steggink.it>
2018-01-23 11:27:57 +0100

    Added script to create all GeoJSON files

---
+++ generate_geojson/generate_all_layers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./export_admin_layer.sh
+./export_infra_layer.sh
+./export_label_layer.sh
+./export_terrain_layer.sh
+./export_urban_layer.sh
+./export_water_layer.sh
+./export_water-line_layer.sh
